b93c19d6aa08475da1e39c0be088ad77dc27eaa2
devohda <fffvvv6@gmail.com>
2022-09-09 14:46:05 +0900

    lecture:35

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
-  return <h2>hi</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car</h2>
+        <div className="expense-item__price">$294</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
