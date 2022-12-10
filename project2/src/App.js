9e1c8e4f32641a601003b48cb943b9ea2075440c
오다혜 <fffvvv6@gmail.com>
2022-12-10 11:33:56 +0900

    chore: prettier 적용

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
--- project2/src/components/MainHeader/Navigation.js
+++ project2/src/components/MainHeader/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = props => {
   return (
     <nav className={classes.nav}>
       <ul>
